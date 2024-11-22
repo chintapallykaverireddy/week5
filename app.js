5bb6c5ca548b2ccc724990401c8080f3d8cbca81
kaverireddy <kaverireddy267@gmail.com>
2024-11-22 15:41:42 +0530

    commit-5

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express=require('express');
 const app=express();
-const PORT=process.env.PORT || 3000;
+const PORT= 3002;
 
 app.get('/',(req,res)=>{
     res.send("Hello Everyone !");
 });
 
-app.listen(3000,'0.0.0.0',()=>{
-    console.log("Server is running on port 3000");
+app.listen(PORT,()=>{
+    console.log("Server is running on local host https:/localhost:3002");
 } );
